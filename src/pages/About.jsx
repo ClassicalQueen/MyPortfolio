3275758bf57be429f10ebecd6467b771cc016eeb
Desiree Chukwuji <queendesi06@gmail.com>
2024-09-20 10:05:22 +0100

    updated resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function About() {
   return (
@@ -8,10 +8,14 @@ function About() {
       <div className="about-section">
         <h1>About Me</h1>
         <p>
-          Hello! I'm Desiree Onyinyechi Chukwuji, a certified and passionate frontend developer with experience
-          in building web applications using React, JavaScript, and other modern technologies.
-          I focus on creating user-friendly, visually engaging, and accessible interfaces.
+          Hello! I'm Desiree Onyinyechi Chukwuji, a certified and passionate
+          frontend developer with experience in building web applications using
+          React, JavaScript, and other modern technologies. I focus on creating
+          user-friendly, visually engaging, and accessible interfaces.
         </p>
+        <a href="/assets/MINE.pdf" download="Desiree_Chukwuji_Resume.pdf">
+          <button class="download-resume-btn">Download My Resume (PDF)</button>
+        </a>
       </div>
       <Footer />
     </section>
